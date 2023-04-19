a6839cfd7e534edca7b74d2a61971772b0b092ba
Smcintosh-greentree <131014067+Smcintosh-greentree@users.noreply.github.com>
2023-04-19 15:50:04 -0400

    Create TestLibrary.py

---
+++ TestLibrary.py
@@ -0,0 +1,10 @@
+def get_sales_performance_feedback(sales_result, sales_target):
+    ratio = int(sales_result) / int(sales_target)
+    if ratio >= 1:
+        return "A great result!"
+    elif ratio > 0.75:
+        return "You did not quite make it, but not too bad."
+    elif ratio >= 0.5:
+        return "Well, at least you tried"
+    else:
+        return "The boss wants to see you..."
