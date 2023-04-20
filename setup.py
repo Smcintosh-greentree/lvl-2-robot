8e41274387ad462cf0952518498fe31cf8ce935d
Smcintosh-greentree <131014067+Smcintosh-greentree@users.noreply.github.com>
2023-04-20 08:54:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mylibrary',
-    version='0.1',
+    version='1.0',
     description='My custom Robot Framework library',
     author='Your Name',
     author_email='your.email@example.com',
