4182079448321a8b3eab3767ea2ec066f2a36efc
Smcintosh-greentree <131014067+Smcintosh-greentree@users.noreply.github.com>
2023-04-21 09:10:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     description='My custom Robot Framework library',
     author='Your Name',
     author_email='your.email@example.com',
-    packages=['Libraries', 'RobotLibrary'],
+    packages=['Libraries'],
     package_data={
-        'RobotLibrary': ['Libraries/RobotLibrary.robot'],
+        'Libraries': ['Libraries/RobotLibrary.robot'],
     },
     install_requires=[
         'robotframework'
